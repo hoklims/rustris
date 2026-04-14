0ca854b0e244839b0d90080d52cfb8b1808e1865
jbfoncin <jeanbaptiste.foncin@gmail.com>
2026-04-14 11:15:38 +0000

    added tetronimo rendering

--- src/render/gamegrid_rendering.rs
+++ src/render/gamegrid_rendering.rs
@@ -1,29 +1,38 @@
 use macroquad::color::BLACK;
-use macroquad::shapes::draw_rectangle;
 
 use crate::gamecore::game_grid::{ GRID_HEIGHT, GRID_WIDTH, GameGrid };
-use crate::render::block_rendering::render_block;
+use crate::gamecore::tetrominos::Coord;
+use crate::render::block_rendering::{ render_block, render_square };
 use crate::render::window::Window;
 
 pub fn render_gamegrid(gamegrid: &GameGrid, window: &Window) -> () {
 
     let gamegrid_display_origin: (f32, f32) = ( window.block_size,
                                                 window.block_size );
+
+    // rendering fixed blocks
     for row in 0..GRID_HEIGHT {
         for col in 0..GRID_WIDTH {
             // gamegrid has bottom left index, macroquad needs top left
             let mq_coord: (f32, f32) = 
                 ( col as f32 * window.block_size + gamegrid_display_origin.0,
-                  (GRID_HEIGHT - row) as f32 * window.block_size + gamegrid_display_origin.1 );
+                  (GRID_HEIGHT - row - 1) as f32 * window.block_size + gamegrid_display_origin.1 );
 
             match gamegrid.grid[row as usize][col as usize] {
                 Some(color) => { render_block(&mq_coord, color, window, false); }
-                None => { draw_rectangle(mq_coord.0,
-                                         mq_coord.1, 
-                                         window.block_size, 
-                                         window.block_size, 
-                                         BLACK) }
+                None => { render_square(&mq_coord, BLACK, window); }
             }
         }
     }
+
+    // rendering tetromino
+    for coord in gamegrid.current_tetromino.mask
+                            .map(|x: Coord| {x + gamegrid.tet_coord}) {
+        let mq_coord: (f32, f32) = 
+            ( coord.x as f32 * window.block_size + gamegrid_display_origin.0,
+             (GRID_HEIGHT - coord.y) as f32 * window.block_size + gamegrid_display_origin.1 );
+
+        render_block(&mq_coord, gamegrid.current_tetromino.color, window, false)    
+    }
+
 }
