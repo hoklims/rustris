c9f885f862488c565e835314e04316b7da52f22a
jbfoncin <jeanbaptiste.foncin@gmail.com>
2026-04-23 07:00:40 +0000

    font is now embedded in the executable

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
-#![allow(dead_code)] // REMOVE THIS BEFORE SHIPPING
-use macroquad::{ prelude::BLACK, text::{Font, load_ttf_font}, window::{ clear_background, next_frame }};
+use macroquad::{ prelude::BLACK, text::{Font, load_ttf_font_from_bytes}, window::{ clear_background, next_frame }};
 mod gamecore;
 mod state;
 mod render;
 use crate::{ gamecore::game_grid::GameGrid, 
-             render::{ background_rendering::render_background, gamegrid_rendering::render_gamegrid, score_rendering::display_score, window::Window },
+             render::{ background_rendering::render_background, 
+                       gamegrid_rendering::render_gamegrid, 
+                       score_rendering::display_score, 
+                       window::Window },
              state::gamegrid_manager::GameGridManager };
+
+const FONT_FILE: &[u8] = include_bytes!("../assets/square_sans_serif_7.ttf");
+
 #[macroquad::main("Rustris")]
 async fn main() {
-    let font: Font = load_ttf_font("assets/square_sans_serif_7.ttf").await.unwrap();
+    let font: Font = load_ttf_font_from_bytes(FONT_FILE).unwrap();
     let mut window: Window = Window::new();
     let mut gamegrid: GameGrid = GameGrid::new();
     let mut gamegrid_manager: GameGridManager = GameGridManager::new();
+
     loop {
+        
         window.refresh_window_if_needed();
         clear_background(BLACK);
         render_background(&window);
@@ -20,5 +27,6 @@ async fn main() {
         render_gamegrid(&gamegrid, &window);
         display_score(&window, gamegrid_manager.score, gamegrid_manager.level, &font);
         next_frame().await
+
     }
 }
