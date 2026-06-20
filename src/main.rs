85a71e2a57120351b29848eecbdf01b955741ab7
jbfoncin <jeanbaptiste.foncin@gmail.com>
2026-06-20 16:08:02 +0000

    finished UI rendering

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #[allow(dead_code)]
 use std::time::SystemTime;
 
-use macroquad::{ prelude::BLACK, 
+use macroquad::{ prelude::BLACK, rand::srand, 
                  text::{ Font, load_ttf_font_from_bytes }, 
-                 window::{ clear_background, next_frame },
-                 rand::srand };
+                 window::{ clear_background, next_frame } };
 mod gamecore;
 mod state;
 mod render;
@@ -16,17 +15,25 @@ use crate::{ gamecore::game_grid::GameGrid,
                        buttons::ButtonRenderer },
              state::gamegrid_manager::GameGridManager };
 
-const FONT_FILE: &[u8] = include_bytes!("../assets/square_sans_serif_7.ttf");
+const FONT_SCORE: &[u8] = include_bytes!("../assets/square_sans_serif_7.ttf");
+
+const ARROW_UP_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_up_gray.png");
+const ARROW_RIGHT_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_right_gray.png");
+const ARROW_DOWN_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_down_gray.png");
+const ARROW_LEFT_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_left_gray.png");
 
 #[macroquad::main("Rustris")]
 async fn main() {
     
     srand(SystemTime::now().elapsed().unwrap().subsec_nanos() as u64);
-    let font: Font = load_ttf_font_from_bytes(FONT_FILE).unwrap();
+    let font_score: Font = load_ttf_font_from_bytes(FONT_SCORE).unwrap();
     let mut window: Window = Window::new();
     let mut gamegrid: GameGrid = GameGrid::new();
     let mut gamegrid_manager: GameGridManager = GameGridManager::new();
-    let button_renderer: ButtonRenderer = ButtonRenderer::new(&font);
+    let button_renderer: ButtonRenderer = ButtonRenderer::new(ARROW_UP_PNG_BYTES,
+                                                              ARROW_RIGHT_PNG_BYTES,
+                                                              ARROW_DOWN_PNG_BYTES,
+                                                              ARROW_LEFT_PNG_BYTES);
 
     loop {
         
@@ -36,7 +43,7 @@ async fn main() {
         button_renderer.render_buttons(&window);
         gamegrid_manager.get_and_apply_player_input(&mut gamegrid).unwrap();
         render_gamegrid(&gamegrid, &window);
-        display_score(&window, gamegrid_manager.score, gamegrid_manager.level, &font);
+        display_score(&window, gamegrid_manager.score, gamegrid_manager.level, &font_score);
         next_frame().await
 
     }
--- src/render/buttons.rs
+++ src/render/buttons.rs
@@ -1,33 +1,52 @@
-use macroquad::color::{ BLACK, GRAY, WHITE };
-use macroquad::text::Font;
-use macroquad::ui::Skin;
-use macroquad::ui::{ root_ui, hash, widgets::Button, Ui, Style };
+use macroquad::color::GRAY;
+use macroquad::prelude::ImageFormat;
+use macroquad::texture::Texture2D;
+use macroquad::ui::{ Skin, widgets, Style };
+use macroquad::ui::{ root_ui, Ui, hash };
 use macroquad::math::{ Vec2, vec2 };
 use crate::render::window::Window;
 
 pub struct ButtonRenderer {
-    skin: Skin
+    button_skin: Skin,
+    up_button_texture: Texture2D,
+    right_button_texture: Texture2D,
+    down_button_texture: Texture2D,
+    left_button_texture: Texture2D
 }
 
 impl ButtonRenderer {
 
-    pub fn new(font: &Font) -> ButtonRenderer {
+    pub fn new(arrow_up_bytes: &[u8],
+               arrow_right_bytes: &[u8],
+               arrow_down_bytes: &[u8],
+               arrow_left_bytes: &[u8]) -> ButtonRenderer {
 
-        let window_style: Style = root_ui().style_builder()
-                                           .color(GRAY)
-                                           .build();
-        let button_style: Style = root_ui().style_builder()
-                                           .color(WHITE)
-                                           .with_font(&font).unwrap()
-                                           .text_color(BLACK)
-                                           .build();
-        let skin = Skin {
-            window_style,
-            button_style,
-            ..root_ui().default_skin()
-        };
+        let button_skin: Skin =  {
 
-        ButtonRenderer{ skin }
+            let button_style: Style = root_ui().style_builder().color(GRAY).build();
+            let window_style: Style = root_ui().style_builder().color(GRAY).build();
+
+            Skin { button_style,
+                   window_style,
+                   ..root_ui().default_skin().clone() }
+            };
+
+        ButtonRenderer {
+
+            button_skin,
+
+            up_button_texture: Texture2D::from_file_with_format(arrow_up_bytes,
+                                                                Some(ImageFormat::Png)), 
+
+            right_button_texture: Texture2D::from_file_with_format(arrow_right_bytes,
+                                                                   Some(ImageFormat::Png)),
+
+            down_button_texture: Texture2D::from_file_with_format(arrow_down_bytes, 
+                                                                  Some(ImageFormat::Png)),
+
+            left_button_texture: Texture2D::from_file_with_format(arrow_left_bytes, 
+                                                                  Some(ImageFormat::Png))
+        }
     }
 
     pub fn render_buttons(&self, window: &Window) -> () {
@@ -35,19 +54,42 @@ impl ButtonRenderer {
         let button_window_position: Vec2 = vec2(window.buttons_area_origin.0,
                                                 window.buttons_area_origin.1);
         
-        let button_window_size: Vec2  = vec2(window.block_size * 6.0,
-                                             window.block_size * 6.0);
+        let button_size: Vec2 = vec2(window.block_size * 2.0 , window.block_size * 2.0);
 
-        let up_button_position: Vec2 = vec2(2.0 * window.block_size, 
-                                            0.0);
+        let window_size: Vec2 = vec2(6.0 * window.block_size, 6.0 * window.block_size);
 
-        root_ui().push_skin(&self.skin);
-        
-        root_ui().window(hash!(), button_window_position, button_window_size, |ui: &mut Ui| {
-            Button::new("^")
-                    .position(up_button_position)
-                    .size(vec2(window.block_size * 2.0 , window.block_size*2.0))
-                    .ui(ui);
+        let up_button_position: Vec2 = vec2(2.0 * window.block_size, 0.0);
+        let right_button_position: Vec2 = vec2(4.0 * window.block_size, 2.0 * window.block_size);
+        let down_button_position: Vec2 = vec2(2.0 * window.block_size, 4.0 * window.block_size);
+        let left_button_position: Vec2 = vec2(0.0, 2.0 * window.block_size);
+
+        root_ui().push_skin(&self.button_skin);
+
+        root_ui().window(hash!(), button_window_position, window_size, |ui: &mut Ui | {
+
+            widgets::Button::new(self.up_button_texture.clone())
+                .position(up_button_position)
+                .size(button_size)
+                .ui(ui);
+
+            widgets::Button::new(self.right_button_texture.clone())
+                .position(right_button_position)
+                .size(button_size)
+                .ui(ui);
+
+            widgets::Button::new(self.down_button_texture.clone())
+                .position(down_button_position)
+                .size(button_size)
+                .ui(ui);
+
+            widgets::Button::new(self.left_button_texture.clone())
+                .position(left_button_position)
+                .size(button_size)
+                .ui(ui);
+                     
         });
+
+        root_ui().pop_skin();
     }
+
 }
