a8430b3ea266709bb87be90030fc56af5bc3b60f
jbfoncin <jeanbaptiste.foncin@gmail.com>
2026-06-22 12:05:18 +0000

    moved game loop out of main function for better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[allow(dead_code)]
 use std::time::SystemTime;
 
 use macroquad::{ prelude::BLACK, rand::srand, 
@@ -7,7 +6,7 @@ use macroquad::{ prelude::BLACK, rand::srand,
 mod gamecore;
 mod state;
 mod render;
-use crate::{ gamecore::game_grid::GameGrid, 
+use crate::{ gamecore::game_grid::{ GameGrid, GridError }, 
              render::{ background_rendering::render_background, 
                        buttons::{ ButtonRenderer, UIAction }, 
                        gamegrid_rendering::render_gamegrid, 
@@ -20,6 +19,25 @@ const ARROW_UP_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_up_gray.png");
 const ARROW_RIGHT_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_right_gray.png");
 const ARROW_DOWN_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_down_gray.png");
 const ARROW_LEFT_PNG_BYTES: &[u8] = include_bytes!("../assets/arrow_left_gray.png");
+
+async fn run_game_loop(window: &mut Window, button_renderer: &ButtonRenderer, 
+                       gamegrid_manager: &mut GameGridManager, 
+                       gamegrid: &mut GameGrid, font_score: &Font) -> Result<(), GridError> {
+
+    loop {
+        
+        window.refresh_window_if_needed();
+        clear_background(BLACK);
+        render_background(&window);
+        let ui_action: Option<UIAction> = button_renderer.render_buttons(&window);
+        gamegrid_manager.apply_ui_input(gamegrid, ui_action)?;
+        gamegrid_manager.get_and_apply_player_input(gamegrid)?;
+        render_gamegrid(&gamegrid, &window);
+        display_score(&window, gamegrid_manager.score, gamegrid_manager.level, &font_score);
+        next_frame().await
+
+    }
+}
 
 #[macroquad::main("Rustris")]
 async fn main() {
@@ -35,16 +53,20 @@ async fn main() {
                                                               ARROW_LEFT_PNG_BYTES);
 
     loop {
+
+        let game_loop_result: Result<(), GridError> = run_game_loop(
+            &mut window, 
+            &button_renderer, 
+            &mut gamegrid_manager, 
+            &mut gamegrid, 
+            &font_score)
+            .await;
         
-        window.refresh_window_if_needed();
-        clear_background(BLACK);
-        render_background(&window);
-        let ui_action: Option<UIAction> = button_renderer.render_buttons(&window);
-        gamegrid_manager.apply_ui_input(&mut gamegrid, ui_action).unwrap();
-        gamegrid_manager.get_and_apply_player_input(&mut gamegrid).unwrap();
-        render_gamegrid(&gamegrid, &window);
-        display_score(&window, gamegrid_manager.score, gamegrid_manager.level, &font_score);
-        next_frame().await
+        match game_loop_result {
+            Err(GridError::TetOutsideGrid) | Ok(()) => { continue; },
+            _ => { game_loop_result.unwrap() }
+        }
 
     }
+
 }
